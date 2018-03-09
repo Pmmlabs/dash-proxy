2750e81c16aaf7a6e7efd3435f9e3ec6eb61c616
pmmlabs <kilowatt36@mail.ru>
2018-03-09 22:56:39 +0300

    delete only if success

--- merge.sh
+++ merge.sh
@@ -11,5 +11,4 @@ dir="$dir/"
 FFMPEG=${FFMPEG:-"ffmpeg"}
 cat ${dir}*init.m4v ${dir}*.m4v > ${dir}${result}.m4v
 cat ${dir}*init.m4a ${dir}*.m4a > ${dir}${result}.m4a
-${FFMPEG} -i ${dir}${result}.m4v -i ${dir}${result}.m4a -c copy $dir$(date +%x_%X).mp4
-rm -rf ${dir}*.m4v ${dir}*.m4a
+${FFMPEG} -i ${dir}${result}.m4v -i ${dir}${result}.m4a -c copy $dir$(date +%x_%X).mp4 && rm -rf ${dir}*.m4v ${dir}*.m4a
